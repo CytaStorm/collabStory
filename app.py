9b3a2b8e3622b51a622fdf32c297e8c7daf3fe6f
ChenFangM <ChenFang.NY@gmail.com>
2022-11-01 09:27:11 -0400

    basics

--- app.py
+++ app.py
@@ -8,14 +8,21 @@ from flask import Flask, render_template, request
 app = Flask(__name__)    #create Flask object
 
 @app.route("/")
-def disp_loginpage():
+def loginpage():
     return render_template( 'login.html' )
 
-@app.route("/auth")
-def authenticate():
-    return render_template( 'adding.html' )
+@app.route("/signup")
+def newplayer():
+    return render_template( 'signup.html' )
 
-    
+@app.route("/auth")
+def addlinepage():
+    return render_template( 'play.html' )
+
+@app.route("/success")
+def storypage():
+    return render_template( 'story.html' )
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
     app.debug = True 
