36e9b272b422143e46022c517d56cb089ad77936
ChenFangM <ChenFang.NY@gmail.com>
2022-11-14 17:35:09 -0500

    Basic completion

--- __init__.py
+++ __init__.py
@@ -7,29 +7,57 @@ from flask import Flask, render_template, request, session
 import sqlite3
 
 ### SETUP ###
-DB_FILE = "logins.db"
+DB_FILE = "tellAll.db"
 db = sqlite3.connect(DB_FILE, check_same_thread=False) #the "check_same_thread=False" is needed to stop errors
 c = db.cursor()
 
-def updateStory(): #returns string story with all story from database
-    list = c.execute('select * from entries').fetchall()
+def updateStory(storyName): #returns string story with all story from database
+    list = c.execute(f'select * from {storyName}').fetchall()
     storyText = ""
     for phrases in list:
         storyText = storyText + "\n" + phrases[1]
     return storyText
 
-def lastEntry():
-    list = c.execute('select * from entries').fetchall()
+def lastEntry(storyName): 
+    list = c.execute(f'select * from {storyName}').fetchall()
     return list[-1][1]
 
-def hasSubmitted(usrID): #returns if the user has already submitted
-    command = f"select submitted from login where userID = {usrID}"
-    submitted = c.execute(command)
-    if submitted == 1:
-        return True
+def hasSubmitted(usrID, storyName): #returns if the user has already submitted
+    command = f"select userID from {storyName} where userID = {usrID}"
+    submit = c.execute(command).fetchall()
+    if len(submit) == 0:
+        return False
+    return True
+
+def getStories():
+    command = 'select name from sqlite_master where type="table"'
+    c.execute(command)
+    db.commit()
+    stories= c.fetchall()
+    return [row[0] for row in stories]
+
+def isAStory(storyName):
+    print(getStories())
+    for story in getStories():
+        if story == storyName:
+            return True
     return False
 
-# CREATING login TABLE in logins.db
+def createStory(storyName):
+    tbleName = storyName
+    parameters = "UserID INT, Line TEXT"
+    command = (f"create table {tbleName} ({parameters})")
+    c.execute(command)
+    db.commit() #save changes
+
+def removeSpaces(string):
+    words = string.split(" ")
+    result = ""
+    for word in words:
+        result = result + word + "_"
+    return result[:-1]
+
+# CREATING login TABLE in tellAll.db
 tbleName = "login"
 parameters = "UserID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Password TEXT, Submitted INTEGER"
 
@@ -37,37 +65,18 @@ command = (f"create table if not exists {tbleName} ({parameters})")
 c.execute(command)
 db.commit() #save changes
 
-# #CREATING User entries TABLE in logins.db
-# tbleName = "entries"
-# parameters = "UserID INT, Line TEXT"
+################################### FLASK #########################################
 
-# """
-# command = "drop table entries"
-# c.execute(command)
-# """
-
-# command = (f"create table if not exists {tbleName} ({parameters})")
-# c.execute(command)
-# db.commit() #save changes
-
-### FLASK ###
 app = Flask(__name__)    #create Flask object
 app.secret_key = b'ekifl@&n&!urniwer7[23[q894;8^'
 
 @app.route("/")
 def routing():
     print("routing...")
+    print(getStories())
     
     if 'user' in session:
-        # if hasSubmitted(session['user']):
-        #     return render_template('addedStory.html', story = updateStory())
-        # else:
-        #     story = updateStory()
-        #     if len(story) == 0:
-        #         return render_template('storyInput.html')
-        #     else:
-        #         return render_template('storyInput.html', line = lastEntry())
-        return render_template('home.html')
+        return render_template('home.html', listOfStories = getStories()[2:])
     return render_template('login.html')
 
 @app.route("/auth", methods=['POST'])
@@ -99,10 +108,7 @@ def authenticate():
         print("password Works")
         session['user'] = combo[0] #Start a session with stored value of UserID
         #Render story page if the user already submitted an entry, otherwise render entry page
-        if hasSubmitted(session['user']): 
-            return render_template('addedStory.html', story = updateStory()) #User has already submitted, so takes to response page
-        else:
-            return render_template('storyInput.html', line = lastEntry())
+        return render_template('home.html', listOfStories = getStories()[2:])
     else:
         print("wrong password")
         return render_template('login.html', error = "Wrong Password") #calls the HTML file with the error
@@ -139,19 +145,44 @@ def register():
 
     return render_template('login.html') # Return to login page
 
+@app.route("/redirectNew", methods=['POST'])
+def toNew():
+    storyName = removeSpaces(request.form['storyName'])
+    if isAStory(storyName):
+        return render_template('home.html', listOfStories = getStories()[2:], error = "Story name not available")
+    else:
+        createStory(storyName) 
+        print("created")
+        session['currStory'] = storyName
+        return render_template('storyInput.html')
+
+@app.route("/redirectOld", methods=['POST'])
+def toOld():
+    storyName = removeSpaces(request.form['selStory'])
+    if isAStory(storyName):
+        session['currStory'] = storyName
+        if hasSubmitted(session['user'], session['currStory']):
+            return render_template('addedStory.html', story = updateStory(session['currStory']))
+        return render_template('storyInput.html', line = lastEntry(session['currStory']))
+    else:
+        return render_template('home.html', listOfStories = getStories()[2:], error = "Story not found")
+
 @app.route("/addedStory", methods=['POST'])
 def addedStory():
 
-    if not(hasSubmitted(session['user'])):
-        print("adding and printing story...")
-        # add entry into the main story
-        newEntry = request.form['newEntry']
-        command = (f"INSERT INTO entries VALUES(\"{session['user']}\", \"{newEntry}\")")
-        c.execute(command)
-        c.execute(f"UPDATE login SET submitted = 1 WHERE userID = {session['user']}") #Update submit status
-        db.commit()
+    print("adding and printing story...")
+    # add entry into the main story
+    newEntry = request.form['newEntry']
+    command = (f"INSERT INTO {session['currStory']} VALUES(\"{session['user']}\", \"{newEntry}\")")
+    c.execute(command)
+    db.commit()
     
-    return render_template('addedStory.html', story=updateStory())
+    return render_template('addedStory.html', story=updateStory(session['currStory']))
+
+@app.route("/returnHome")
+def returning():
+    session.pop('currStory')
+    return render_template('home.html', listOfStories = getStories()[2:])
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
